aacd86f50579e08f8be8960f895e1a75e42a4394
HongSukYoung <145235252+HongSukYoung@users.noreply.github.com>
2024-07-17 16:32:06 +0900

    Update cameraclient.py

--- 07_11_test/cameraclient.py
+++ 07_11_test/cameraclient.py
@@ -3,7 +3,7 @@ import threading
 import camera  # camera 모듈을 가져옵니다
 
 # 서버 정보를 설정합니다.
-HOST = '127.0.0.1'  # 서버의 주소 (여기서는 로컬호스트)
+HOST = '192.168.118.242'  # 서버의 주소 (여기서는 로컬호스트)
 PORT = 65439  # 서버의 포트 번호
 
 def receive_messages(sock):
@@ -25,10 +25,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         message = input("Enter message to send (type 'exit' to quit): ")
 
         if message == 'take':
+            s.sendto(message.encode())
             coner_point, box_height = camera.take_photo()
             # 받은 값을 문자열로 변환하여 서버로 전송합니다
             data_to_send = f"coner_point: {coner_point}, box_height: {box_height}"
+            #s.sendall(message.encode())ex
             s.sendall(data_to_send.encode())
+
         elif message == 'exit':
             break
         else:
